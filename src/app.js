287f195c2705b6e23587b93c97228a4def8f2ef2
Richard Le <rle@mp.ics.com>
2021-03-08 18:38:00 -0800

    init commit

---
+++ src/app.js
@@ -0,0 +1,108 @@
+const path = require('path')
+
+const express = require('express')
+const hbs = require('hbs') // for partials
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+
+//Define paths for express cofig
+const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,"../templates/views")
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+const app = express() 
+
+//setup handlebars and views location
+app.set('view engine', 'hbs') //set up handlebars
+app.set('views',viewsPath) //views path
+hbs.registerPartials(partialsPath)
+
+//set up static direcotry to server
+app.use(express.static(publicDirectoryPath))
+
+
+//domain routing 
+
+app.get('',(req,res) => {
+    res.render('index',{
+        title: "weather app",
+        name: "richard le"
+    })
+})
+
+//somehow knows how to get to views
+app.get('/about',(req,res)=> {
+    res.render('about', {
+        title:"weather",
+        name:"richard le"
+    })
+})
+
+app.get('/help', (req,res)=> {
+    res.render('help',{
+        help: "you need help",
+        title: "help",
+        name: "richard le"
+    })
+})
+
+app.get('/products',(req,res)=>{
+    if (!req.query.search){
+        return res.send({
+            error:"you must provide search term."
+        })
+    }
+    console.log(req.query)
+    res.send({
+        products:[]
+    })
+})
+
+app.get('/weather',(req,res)=> {
+    if(!req.query.address){
+        return res.send({
+            error: "no address provided"
+        })
+    }
+
+    geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
+        if (error){
+            return res.send({error})
+        }
+        console.log(latitude,longitude)
+        forecast(latitude,longitude,(error,forecastData)=>{
+            if (error){
+                return res.send({error})
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+    // res.send({
+    //     address: req.query.address
+    // })
+})
+
+app.get("/help/*",(req,res)=>{
+    res.render('error',{
+        title:'404',
+        name:"richard le",
+        errorMessage: "help article not found",
+    })
+})
+
+app.get("*",(req,res)=>{
+    res.render('error',{
+        title: '404',
+        name : "richard le",
+        errorMessage: "Page not found",
+    })
+})
+
+app.listen(3000, ()=>{
+    console.log("server is up on port 3000")
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,33 @@
+const request = require("request")
+
+// const url = "http://api.weatherstack.com/current?access_key=bd3663f18bfd56cef2ff19a8419e4d61&query=37.8267,-122.4233&units=f"
+
+
+//                               //lower level | response for bad queries
+// request({url: url,json:true}, (error,response)=>{
+//     if (error)
+//     {
+//         console.log("unable to connect")
+//     } else if(response.body.error){
+//         console.log("bad query")
+//     }else{
+//     console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature +" degrees out. There is a " + response.body.current.precip + "% chance of rain")
+//     }
+// })
+
+const forecast = (lat,long,callback) => {
+    const url = "http://api.weatherstack.com/current?access_key=bd3663f18bfd56cef2ff19a8419e4d61&query=" + lat +',' +long+"&units=f"
+    request({url,json:true}, (error,{body}) => {
+        if(error){
+            callback("no internet connection",undefined)
+           // console.log("no internet connection")
+        } else if(body.error){
+            callback("bad query. try again", undefined)
+        } else{
+            callback(undefined,body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature +" degrees out. There is a " + body.current.precip + "% chance of rain")
+            //console.log(response)
+        }
+    })
+}
+
+module.exports = forecast
